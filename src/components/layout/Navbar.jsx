f4308352dae023b1b43aa16d35cf96e49f1da305
FahmiMoch <fahmifadillah18@gmail.com>
2026-06-04 23:49:32 +0700

    Menambahkan Link

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -9,6 +9,7 @@ export default function Navbar() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/tracking">Tracking</Link>
+        <Link to="/service">Service</Link>
         <Link to="/contact">Contact</Link>
       </div>
 
