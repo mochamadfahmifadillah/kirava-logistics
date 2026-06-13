3abd2ff2e53d0056efebaf1616a6be1063451b57
FahmiMoch <fahmifadillah18@gmail.com>
2026-06-13 23:14:22 +0700

    Menambahkan navbar pada bagian tracking

--- src/pages/Tracking.jsx
+++ src/pages/Tracking.jsx
@@ -2,24 +2,28 @@ import { useState } from "react";
 import { getTracking } from "../features/tracking/trackingAPI";
 import Input from "../components/ui/Input";
 import Button from "../components/ui/Button";
+import Navbar from "../components/layout/Navbar";
 
 export default function Tracking() {
   const [id, setId] = useState("");
 
   return (
-    <div className="min-h-screen bg-[#0A1F44] text-white flex flex-col items-center justify-center">
-      
-      <h1 className="text-3xl mb-6">Track Your Shipment</h1>
+    <>
+      <Navbar />
 
-      <div className="flex gap-3">
-        <Input
-          placeholder="Enter tracking ID"
-          value={id}
-          onChange={(e) => setId(e.target.value)}
-        />
+      <div className="min-h-screen bg-[#0A1F44] text-white flex flex-col items-center justify-center">
+        <h1 className="text-3xl mb-6">Track Your Shipment</h1>
 
-        <Button>Track</Button>
+        <div className="flex gap-3">
+          <Input
+            placeholder="Enter tracking ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+
+          <Button>Track</Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
