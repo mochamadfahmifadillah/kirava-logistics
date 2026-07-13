de5390aa39b03a181921bf49aafc52f747b5cb23
FahmiMoch <fahmifadillah18@gmail.com>
2026-07-13 23:55:57 +0700

    Membetulkan alamat image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,7 +50,7 @@ export default function Home() {
         {/* RIGHT */}
         <div className="flex justify-center">
           <img
-            src="/assets/images/truck.png"
+            src="/assets/truck-image.jpg"
             alt="truck"
             className="w-full max-w-[600px] drop-shadow-[0_0_40px_rgba(59,130,246,0.4)]"
           />
